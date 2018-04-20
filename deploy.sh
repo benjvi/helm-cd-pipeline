43b96ac9a9394330abc640bf9c9f70606de3adf9
Ben Vickers <ben.vickers@opencredo.com>
2018-04-20 12:01:32 +0100

    Move deployable charts into subdirectory

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ diff_base_ref=${2:-master}
 function get_release_diff() {
   local chart_name=$1
   local release="${chart_name}-${target_ns}"
-  if [ -e "${chart_name}/values-${target_ns}.yml" ]; then
-    RELEASE_ARGS="--values ${chart_name}/values-${target_ns}.yml"
+  if [ -e "charts/${chart_name}/values-${target_ns}.yml" ]; then
+    RELEASE_ARGS="--values charts/${chart_name}/values-${target_ns}.yml"
   else
     RELEASE_ARGS=""
   fi
 
   # this var cant be local, $? doesnt work
-  diff_output=$(helm diff $RELEASE_ARGS "${release}" "${chart_name}/chart/" 2> "/tmp/${target_ns}-diff-error" )
+  diff_output=$(helm diff $RELEASE_ARGS "${release}" "charts/${chart_name}/chart/" 2> "/tmp/${target_ns}-diff-error" )
   local diff_success=$?
   local diff_err=$(cat "/tmp/${target_ns}-diff-error")
   local diff_len=$(printf "$diff_output" | wc -l | tr -d " ")
@@ -49,8 +49,8 @@ for chart_name in ${modified_charts_git}; do
   if [ -n "$release_diff" ]; then
     printf "Release diff: \n${release_diff}\n"
     echo "Package for release \"$release\" is different than the currently deployed version, upgrading..."
-    if [ -e "$chart_name/values-${target_ns}.yml" ]; then
-      RELEASE_ARGS="--values $chart_name/values-${target_ns}.yml"
+    if [ -e "charts/$chart_name/values-${target_ns}.yml" ]; then
+      RELEASE_ARGS="--values charts/$chart_name/values-${target_ns}.yml"
     else
       RELEASE_ARGS=""
     fi
@@ -60,7 +60,7 @@ for chart_name in ${modified_charts_git}; do
     # prefer not to force or purge (probably there are resources we shouldn't destroy)
     # this will also fail if a previous revision failed and was not rolled back
     # TODO: try to rollback to previous version on failure (this can still fail if its the first release)
-    helm upgrade --install --wait --timeout 120 --namespace "${target_ns}" $RELEASE_ARGS "${release}" "$chart_name/chart/" || true 
+    helm upgrade --install --wait --timeout 120 --namespace "${target_ns}" $RELEASE_ARGS "${release}" "charts/$chart_name/chart/" || true 
   else
     printf "Deployed release \"$release\" is already up to date, skipping it\n"
   fi
