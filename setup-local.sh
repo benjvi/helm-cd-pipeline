b2afcccb97b0d1ee54db08e8f0b0083cc35dc5c8
Ben Vickers <ben.vickers@opencredo.com>
2018-04-20 14:23:49 +0100

    increase minikube size and configure jenkins

--- setup-local.sh
+++ setup-local.sh
@@ -1,9 +1,7 @@
 # assume helm is already set up with the latest (HEAD) of kube-diff installed
-minikube start
+minikube start --cpus 4 --memory 8192
 kubectl create ns infra
 helm init --wait
 # TODO pipeline plugin needs upgrading for multibranch plugin to work
 # also will need to create test jobs manually (and git creds)
-helm install --name jenkins \
-      --namespace jenkins \
-            jenkins/
+helm install --name jenkins --namespace jenkins --values jenkins/values-override.yml jenkins/
